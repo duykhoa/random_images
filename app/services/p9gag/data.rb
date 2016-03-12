fcf4f7c5156657513197f5b3adc59e6e29ee02ff
Duy Khoa <duykhoa12t@gmail.com>
2016-03-12 22:56:06 +0800

    Remove delete all p9gag links

--- app/services/p9gag/data.rb
+++ app/services/p9gag/data.rb
@@ -3,9 +3,6 @@ module P9gag
     def store(raw_data)
       json_data = JSON.parse(raw_data)
 
-      # clear_9gag link
-      P9gagLink.delete_all
-
       json_data["data"].each do |json|
         P9gagLink.create(link: json["images"]["large"])
       end
